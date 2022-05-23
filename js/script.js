b84aceaf52f17e885ef4db740f28d5a8e2e119a5
1enablex <igor.marines25@gmail.com>
2022-05-22 23:41:43 -0300

    alteração background de acordo com horario local

--- js/script.js
+++ js/script.js
@@ -1,35 +1,55 @@
-/* JavaScript com Jquery */
-
-
 $(document).ready(function () {
-    /* Animações inicio 
-       Formulário
-    */
-    $("#portfolio").click(function () {
+  $("#portfolio").click(function () {
       $("#carroselshow").slideToggle("slow", function () {
-        //Se precisar executar algo após o carregamento da animação colocar aqui.
-        $("").slideToggle();
-      });
-    });
-  
-    $("#botaoenviar").click(function () {
-      $(".containerglassmorph").slideToggle("slow", function () {
-        //Se precisar executar algo após o carregamento da animação colocar aqui.
-        $('#cliqueAqui').show()
-        $('#divForm').hide()
-      });
-    });
-    /* Animações Tópico Enviado */
-    $("#cliqueAqui2").click(function () {
-      $("#divForm").slideToggle("slow", function () {
-        //Se precisar executar algo após o carregamento da animação colocar aqui.
-        $("#compartilhe").slideToggle();
-        $('.containerglassmorph').hide()
-        $('#compartilhe').hide()
-        $('#cliqueAqui').slideToggle(function () {
-  
-        });
-      });
-    });
-    /* Animações final */
+          //Se precisar executar algo após o carregamento da animação colocar aqui.
+          $(".homeshow").slideToggle();
+      });                
   });
+});
+$(document).ready(function () {
+  $("#sobre").click(function () {
+      $(".sobreshow1").slideToggle("slow", function () {
+          //Se precisar executar algo após o carregamento da animação colocar aqui.
+          $(".homeshow").slideToggle();
+      });
+      $("#carroselshow").hide();
+  });
+});
+$(document).ready(function () {
+  $("#home").click(function () {
+      $(".homeshow").show();
+      $("#carroselshow").hide();
+      $(".sobreshow1").hide();
+  });
+});
+
+const dark = document.getElementById('dark')
+const back = document.body
+const btnChange = document.querySelectorAll('.pulse')
+dark.addEventListener('click', () => {
+  dark.classList.toggle('dark')
+  back.classList.toggle('dark')
+  btnChange.forEach(elemento => elemento.classList.toggle('btn-change'))
+})
+
+// fundo de acordo com horario
+
+const data = new Date() // recebe data pc
+const hours = data.getHours() // recebe horas da data atual
+
+function changeBackground(hx){
+  document.querySelector('body').style.backgroundColor = hx
+}
+
+if(hours <= 12) {
+  // document.querySelector('body').style.backgroundColor = `#d2f9f9`
+  // document.querySelector('body').style.color = `#80df20`
+  
+  changeBackground(`#FAF7FD`)
+}else if(hours <= 18){
+  changeBackground(`#b7adeb`)
+}else if(hours <= 23){
+  changeBackground(`#050000`)
+}
+
+console.log(hours)
