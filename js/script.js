0093ca127629d1c611c9562bbc5d106235aaa68f
Igor Marines <igor.marines25@gmail.com>
2022-06-13 23:16:55 -0300

    Atualização Portfolio

--- js/script.js
+++ js/script.js
@@ -1,55 +1,32 @@
 $(document).ready(function () {
-  $("#portfolio").click(function () {
-      $("#carroselshow").slideToggle("slow", function () {
-          //Se precisar executar algo após o carregamento da animação colocar aqui.
-          $(".homeshow").slideToggle();
-      });                
-  });
+    $("#portfolio").click(function () {
+        $(".conteudo-sobre").slideToggle("slow", function () {
+            //Se precisar executar algo após o carregamento da animação colocar aqui.
+            $(".conteudo-principal").slideToggle();
+        });
+    });
+    $("#contato").click(function(){
+        $(".forma-contato").slideToggle("slow", function(){
+            
+        });
+    });
+    $("#Foto-Anime").click(function () {
+        $("#Foto-Human").slideToggle("fast", function (){
+            $("#Foto-Anime").slideToggle(); 
+        });
+    });
+    $("#Foto-Human").click(function () {
+        $("#Foto-Anime").slideToggle("fast", function (){
+            $("#Foto-Human").slideToggle(); 
+        });
+    });
 });
-$(document).ready(function () {
-  $("#sobre").click(function () {
-      $(".sobreshow1").slideToggle("slow", function () {
-          //Se precisar executar algo após o carregamento da animação colocar aqui.
-          $(".homeshow").slideToggle();
-      });
-      $("#carroselshow").hide();
-  });
-});
-$(document).ready(function () {
-  $("#home").click(function () {
-      $(".homeshow").show();
-      $("#carroselshow").hide();
-      $(".sobreshow1").hide();
-  });
-});
 
-const dark = document.getElementById('dark')
-const back = document.body
-const btnChange = document.querySelectorAll('.pulse')
-dark.addEventListener('click', () => {
-  dark.classList.toggle('dark')
-  back.classList.toggle('dark')
-  btnChange.forEach(elemento => elemento.classList.toggle('btn-change'))
-})
-
-// fundo de acordo com horario
-
-const data = new Date() // recebe data pc
-const hours =data.getHours() // recebe horas da data atual
-
-function changeBackground(hx){
-  document.querySelector('body').style.backgroundColor = hx
-}
-
-if(hours <= 12) {
-  // document.querySelector('body').style.backgroundColor = `#d2f9f9`
-  // document.querySelector('body').style.color = `#80df20`
-  
-  changeBackground(`#050000`)
-
-}else if(hours <= 18){
-  changeBackground(`#b7adeb`)
-
-}else if(hours <= 23){
-  changeBackground(`#FAF7FD`)
+function DarkMode() {
+    var darkTheme = document.body;
+    var darkBorder = document.querySelector(".conteudo-principal")
+    var darkBorder2 = document.querySelector(".conteudo-sobre")
+    darkTheme.classList.toggle("dark-mode");
+    darkBorder.classList.toggle("dark-border");
+    darkBorder2.classList.toggle("dark-border");
 }
